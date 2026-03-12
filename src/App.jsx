e8b9ba24cfdd90bcc1650dab352d3facbd92a284
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2026-03-12 14:02:08 +0800

    install daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-
-import './App.css'
-
 function App() {
-
   return (
     <>
-      tes
+      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
